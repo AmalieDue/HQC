d0d325164b8d6fbc085e821404f056da620a3262
Amalie Due Jensen <amalieduejensen@hotmail.com>
2021-11-30 11:01:04 +0100

    added test bash script

---
+++ SageMath_submit.sh
@@ -0,0 +1,9 @@
+#BSUB -J SageMath
+#BSUB -q hpc
+#BSUB -n 4
+#BSUB -R "rusage[mem=2GB]"
+#BSUB -R "span[hosts=1]"
+#BSUB -R "select[avx2]"
+#BSUB -W 1:00
+
+/appl/SageMath/9.4/sage test_script.py
---
+++ test_script.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import time
+import pickle
+
+current_time = time.strftime("%m.%d.%y %H:%M:%S", time.localtime())
+output_name = 'test %s.pkl' % (current_time)
+
+F = GF(5)
+
+with open(output_name, "wb") as f:
+    pickle.dump([F(i) for i in range(10)] ,f)
+
+with open(output_name, "rb") as f:
+    print(pickle.load(f))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
